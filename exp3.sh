d7b1d86226f6774fe5f5eb5af499ce84524048ad
Vishnu <vishnusreepadammahe@hotmail.com>
2017-04-11 14:55:05 +0530

    HI

---
+++ exp3.sh
@@ -0,0 +1,15 @@
+echo "Current logged user and his logname : ";
+whoami;
+echo "Current shell : ";
+echo $SHELL;
+echo "Home Directory : ";
+echo $HOME;
+echo "Operating system : ";
+uname -s;
+uname -v;
+echo "Current Path setting : ";
+echo $PATH;
+echo "Current working directory : ";
+pwd
+echo "Currently logged number of users : ";
+users;
